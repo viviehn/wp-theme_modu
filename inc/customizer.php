eca3acc0266d4475ef998019d1875eb7c8949bb0
Vivien <vivng6991@gmail.com>
2016-12-28 21:00:23 -0800

    Include base theme (_s)

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * modu Theme Customizer
+ *
+ * @package modu
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function modu_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'modu_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function modu_customize_preview_js() {
+	wp_enqueue_script( 'modu_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'modu_customize_preview_js' );
